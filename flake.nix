58b240eac4d109f36184cae3a80e0d9cb28e4991
Maxine Alexander <25956545+MaxTheMooshroom@users.noreply.github.com>
2023-08-02 00:55:36 -0400

    fix flake and update version in Makefile

--- flake.nix
+++ flake.nix
@@ -3,24 +3,26 @@
 
   inputs = {
     # what nixos version is this?
-    # nixpkgs.url = "github:nixos/nixpkgs/nixos-xx.xx";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
 
-    dtc.url = "github:MaxTheMooshroom/dtc/dev";
+    # dtc.url = "github:MaxTheMooshroom/dtc/dev";
   };
 
-  outputs = { self, nixpkgs, dtc }: {
+  outputs = { self, nixpkgs }: {
     packages.x86_64-linux.deviceTreeCompiler = let
-      src = dtc;
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
     in
-    nixpkgs.stdenv.mkDerivation {
+    pkgs.stdenv.mkDerivation {
       name = "deviceTreeCompiler-1.8.0";
       
+      src = ./.;
+
       buildInputs = [
-        nixpkgs.gnumake
-        nixpkgs.gcc
-        nixpkgs.flex
-        nixpkgs.bison
-        nixpkgs.pkgconfig
+        pkgs.gnumake
+        pkgs.gcc
+        pkgs.flex
+        pkgs.bison
+        pkgs.pkgconfig
       ];
 
       buildPhase = ''
@@ -30,14 +32,14 @@
       installPhase = ''
         mkdir -p $out/bin
         cp dtc $out/bin/dtc
-      '';
+      ''; # try doing something maybe?
 
       checkPhase = ''
         make tests
         $out/bin/dtc --version
       '';
 
-      meta = with nixpkgs.lib; {
+      meta = with pkgs.lib; {
         description = "DeviceTree Compiler v1.8.0 - Fork by MtM";
         license = licenses.gpl2;
         maintainers = [ "Maxine Alexander <max.alexander3721@gmail.com>" ];
@@ -45,6 +47,6 @@
       };
     };
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.dtc;
+    packages.x86_64-linux.default = self.packages.x86_64-linux.deviceTreeCompiler;
   };
 }
